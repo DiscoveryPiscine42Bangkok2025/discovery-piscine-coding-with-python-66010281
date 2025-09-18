9b26ff6b8e9d35d0bf18aac601f355afc507be83
66010281 <66010281@kmitl.ac.th>
2025-09-18 15:40:31 +0700

    Create scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,9 @@
+import sys
+import re
+if len(sys.argv) < 3:
+    print("none")
+else:
+    word = sys.argv[1]
+    text = sys.argv[2]
+    matches = re.findall(rf'\b{re.escape(word)}\b', text)
+    print(len(matches))
